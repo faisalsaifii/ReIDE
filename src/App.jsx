51b3c78ce1107238880351a3ac77ee1e1ab7d45e
faisalsaifii <m.faisal.s@outlook.com>
2023-11-29 18:26:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <textarea name='input' />
+      <textarea name='output' />
+      <select>
+        <option>Python</option>
+      </select>
+    </>
+  )
+}
+
+export default App
