0e9cfc770927bd1b5a46ff3e771387b38f69086a
faisalsaifii <m.faisal.s@outlook.com>
2023-11-30 11:17:18 +0530

    feat: nav buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [showCodeWindow, setShowCodeWindow] = useState(true)
+  const [showInputWindow, setShowInputWindow] = useState(true)
+  const [showOutputWindow, setShowOutputWindow] = useState(true)
+
   const [code, setCode] = useState(localStorage.getItem('code') || '')
   const [input, setInput] = useState(localStorage.getItem('input') || '')
   const [output, setOutput] = useState(localStorage.getItem('output') || '')
   const [languageId, setLanguageId] = useState(
-    localStorage.getItem('language_id') || 2
+    localStorage.getItem('language_id') || 71
   );
 
   useEffect(() => {
@@ -54,23 +58,41 @@ const App = () => {
 
   return (
     <>
-      <div className='left'>
-        <textarea name='input' className='code-box' value={code} onChange={e => setCode(e.target.value)} placeholder='Code here' />
+      <div className='top'>
+        <button className='nav-btn' onClick={() => setShowCodeWindow(!showCodeWindow)}>Code</button>
+        <button className='nav-btn' onClick={() => setShowInputWindow(!showInputWindow)}>Input</button>
+        <button className='nav-btn' onClick={() => setShowOutputWindow(!showOutputWindow)}>Output</button>
       </div>
-      <div className='right'>
-        <textarea name='output' className='output-box' value={output} onChange={e => setOutput(e.target.value)} placeholder='Output' />
-        <textarea name='input' className='input-box' value={input} onChange={e => setInput(e.target.value)} placeholder='Input' />
-        <div>
-          <select onChange={e => setLanguageId(e.target.value)} value={languageId}>
-            <option value='71'>Python</option>
-            <option value='54'>C++</option>
-            <option value='50'>C</option>
-            <option value='62'>Java</option>
-          </select>
-          <button onClick={handleSubmit}>Run</button>
-        </div>
+      <div className='container'>
+        {
+          showCodeWindow && (<div className='left'>
+            <textarea name='input' className='code-box' value={code} onChange={e => setCode(e.target.value)} placeholder='Code here' />
+          </div>)
+        }
 
+        <div className='right'>
+
+          {
+            showOutputWindow && (<textarea name='output' className='output-box' value={output} onChange={e => setOutput(e.target.value)} placeholder='Output' />)
+          }
+          {
+            showInputWindow && (<textarea name='input' className='input-box' value={input} onChange={e => setInput(e.target.value)} placeholder='Input' />)
+          }
+
+          <div>
+            <select onChange={e => setLanguageId(e.target.value)} value={languageId}>
+              <option value='71'>Python</option>
+              <option value='54'>C++</option>
+              <option value='50'>C</option>
+              <option value='62'>Java</option>
+            </select>
+            <button onClick={handleSubmit}>Run</button>
+
+          </div>
+
+        </div>
       </div>
+
 
     </>
   )
